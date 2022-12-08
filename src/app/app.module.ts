5aebb574960fce94b585077587c547d7907e928d
sruthy-nr <sruthyn.@nestgroup.net>
2022-12-08 07:54:45 +0530

    nav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,34 @@ import { AddProductComponent } from './add-product/add-product.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from "@angular/common/http";
 import { ViewProductComponent } from './view-product/view-product.component';
+import { NavComponent } from './nav/nav.component';
+import { RouterModule, Routes } from '@angular/router';
 
+const myRoute:Routes=[
+  
+  {
+    path:"",
+    component:AddProductComponent
+  },
+  {
+    path:"view",
+    component:ViewProductComponent
+  }
+
+]
 @NgModule({
   declarations: [
     AppComponent,
     AddProductComponent,
-    ViewProductComponent
+    ViewProductComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(myRoute)
   ],
   providers: [],
   bootstrap: [AppComponent]
